25ca66f4a9dabdba0451752a77d5a9f26de04ec9
EDmatrix1 <emanuel.diktonius@gmail.com>
2026-01-06 12:14:34 +0100

    added DTO

---
+++ Version2/src/jdbc/DTO/TeachingDTO.java
@@ -0,0 +1,13 @@
+package jdbc.DTO;
+
+public class TeacherDTO {
+    public record Case1Result(double plannedCost, double actualCost) {}
+
+    public record Case2Result(int before, int after, double costBefore, double costAfter) {}
+
+    public record AllocationResult(boolean success, String message) {}
+
+    public record DeallocationResult(boolean success, String message) {}
+
+    public record ExerciseResult(boolean success, String message) {}
+}
